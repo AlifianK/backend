8e45ff27e28931a8af16c7aeb694ffb1da71d559
Alifian Khalid <alifiancaesar@gmail.com>
2026-04-22 00:14:31 +0700

    add user method

--- src/models/user.rs
+++ src/models/user.rs
@@ -13,3 +13,22 @@ pub struct CreateUser {
     pub name: String,
     pub email: String,
 }
+
+impl User {
+    pub async fn find_by_id(pool: &SqlitePool, id: i64) -> sqlx::Result<Self> {
+        sqlx::query_as!(Self, "SELECT * FROM users WHERE id = ?", id)
+            .fetch_one(pool)
+            .await
+    }
+
+    pub async fn create(pool: &SqlitePool, data: CreateUser) -> sqlx::Result<Self> {
+        sqlx::query_as!(
+            Self,
+            "INSERT INTO users (name, email) VALUES (?, ?) RETURNING *",
+            data.name,
+            data.email
+        )
+        .fetch_one(pool)
+        .await
+    }
+}
