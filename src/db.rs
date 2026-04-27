a27ed7e9a2e29bbd3a8d163d7d3c9576993f0674
Alifian Khalid <alifiancaesar@gmail.com>
2026-04-27 10:00:27 +0700

    add pragma to enforce constraint

--- src/db.rs
+++ src/db.rs
@@ -3,7 +3,14 @@ use sqlx::{SqlitePool, sqlite::SqlitePoolOptions};
 pub async fn connect() -> SqlitePool {
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqlitePoolOptions::new()
-        .max_connections(5)
+        .after_connect(|conn, _| {
+            Box::pin(async move {
+                sqlx::query("PRAGMA foreign_keys = ON")
+                    .execute(conn)
+                    .await?;
+                Ok(())
+            })
+        })
         .connect(&url)
         .await
         .expect("Failed to connect")
